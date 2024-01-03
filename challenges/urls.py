028274cb8c5016d19c4194c4e96e39027105ca90
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2024-01-03 15:15:21 -0600

    final commit

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge , name="month-challenge")
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
-
